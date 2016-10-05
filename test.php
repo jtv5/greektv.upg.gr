6b0bb5166452bd91f1cbab0cee3224c942b72cbf
Ioannis Kokkinis <info@upg.gr>
2016-10-06 00:51:51 +0300

    Updated test.php.

---
+++ test.php
@@ -0,0 +1,7 @@
+<video id="example_video_2" class="video-js vjs-default-skin"
+  controls preload="auto" width="640" height="264"
+  data-setup='{"techOrder": ["viblastflash", "html5"]}'
+  data-viblast-key="N8FjNTQ3NDdhZqZhNGI5NWU5ZTI=">
+ <source src="http://94.23.218.186:1935/cretatv/cretatv/playlist.m3u8" type='application/x-mpegURL' />
+ <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+</video>
