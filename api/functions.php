cc9b888b75c64d3b4a86d16eb8d02a7ae3510242
Ioannis Kokkinis <info@upg.gr>
2016-10-05 13:36:22 +0300

    Updated functions.php.

--- api/functions.php
+++ api/functions.php
@@ -220,8 +220,8 @@ function startapi()
         echo '';
         break;
     case 'vlc':
-  //  header('Content-Type: audio/mpegurl;');
-  header('Content-Type: text');
+    header('Content-Type: audio/mpegurl;');
+  //header('Content-Type: text');
      db_connect();
         echo "#EXTM3U\r\n";
         echo db_select("select * from content where active = '1'  order by ord desc", 'vlc');
