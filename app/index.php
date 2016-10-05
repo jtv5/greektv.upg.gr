5b98de79126786dcdc8af50dab10379613bb3454
Ioannis Kokkinis <info@upg.gr>
2016-10-05 22:03:15 +0300

    Updated index.php.

--- app/index.php
+++ app/index.php
@@ -28,7 +28,9 @@
 		</div>
 	</div>
 
-
+  <div id="footer">
+      Sticky Footer
+  </div>
 
 <script type="text/javascript">
 $(document).ready(function(){
@@ -41,6 +43,41 @@ $(document).ready(function(){
       dataType: 'html'
   });
 
+
+  $(window).bind("load", function() {
+
+         var footerHeight = 0,
+             footerTop = 0,
+             $footer = $("#footer");
+
+         positionFooter();
+
+         function positionFooter() {
+
+                  footerHeight = $footer.height();
+                  footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+
+                 if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+                     $footer.css({
+                          position: "absolute"
+                     }).animate({
+                          top: footerTop
+                     })
+                 } else {
+                     $footer.css({
+                          position: "static"
+                     })
+                 }
+
+         }
+
+         $(window)
+                 .scroll(positionFooter)
+                 .resize(positionFooter)
+
+  });
+
+  
 });
 </script>
 </body>
