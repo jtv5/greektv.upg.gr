eb702b85258acd942014b94b7166bf1fdd022ee7
Ioannis Kokkinis <info@upg.gr>
2016-10-05 20:52:33 +0300

    Updated index_user_scripts.js.

--- mob/js/index_user_scripts.js
+++ mob/js/index_user_scripts.js
@@ -21,30 +21,3 @@
     }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
-
-   function playVideo()
-        {
-            //this function launches default video player and plays the video.mp4 file.
-            intel.xdk.player.playPodcast("http://labs.qnimate.com/video.mp4");
-        }
-
-        document.addEventListener("intel.xdk.player.podcast.start", function(){
-            //started playing video
-         //   alert("start");
-
-        }, false);
-
-        document.addEventListener("intel.xdk.player.podcast.stop", function(){
-            //video finished or player closed
-        //    alert("stop");
-        }, false);
-
-        document.addEventListener("intel.xdk.player.podcast.error", function(){
-            //error occured therefore video cannot be played
-      //      alert("error");
-        }, false);
-
-        document.addEventListener("intel.xdk.player.podcast.busy", function(){
-            //another video is already being played
-       //     alert("busy");
-        }, false);
