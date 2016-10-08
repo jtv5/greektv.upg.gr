2cc9c787c490e52b965b6800d0a21e923f27e3f3
Ioannis Kokkinis <info@upg.gr>
2016-10-09 00:47:35 +0300

    Updated streams.js.

--- admin/streams.js
+++ admin/streams.js
@@ -63,11 +63,6 @@ jQuery(document).ready(function() {
   function onSignIn(googleUser) {
   var table = $('#datatable-buttons').DataTable();
   var ajaxurl = table.ajax.url();
-  var profile = googleUser.getBasicProfile();
-   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-   console.log('Name: ' + profile.getName());
-   console.log('Image URL: ' + profile.getImageUrl());
-   console.log('Email: ' + profile.getEmail());
    }
 
 $('#datatable-buttons').on('click', '.formedit', function(e){
