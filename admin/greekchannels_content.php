4cde3dc10c19d892a997b8228abb78fb5b42ab51
Ioannis Kokkinis <info@upg.gr>
2016-10-09 01:08:14 +0300

    Updated greekchannels_content.php.

--- admin/greekchannels_content.php
+++ admin/greekchannels_content.php
@@ -93,7 +93,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
-                            <button type="submit" value="Submit"  class="btn btn-info waves-effect waves-light">Add Stream</button>
+                            <button type="submit" value="Submit"  class="btn btn-info waves-effect waves-light">Add channel</button>
                         </div>
                         </form>
                     </div>
@@ -106,57 +106,71 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                            <h4 class="modal-title">Edit a channel.</h4>
+                            <h4 class="modal-title">Edit a  channel.</h4>
                         </div>
                         <div class="modal-body">
                           <form id="formedit" action="">
-                          <div class="row">
-                              <div class="col-md-12">
-                                  <div class="form-group">
-                                    <label for="field-channel-e" class="control-label">Channel</label>
-                          <select id="field-channel-e" class="form-control">
 
-                          </select>
-                          </div>
-                        </div>
-                      </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                      <label for="field-title-e" class="control-label">Channel Title</label>
+                                        <input type="text" class="form-control" id="field-title-e">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                      <label for="field-region-e" class="control-label">Region</label>
+                            <select id="field-region-e" class="form-control">
+                              <option  value="Greece - Nationwide">Greece - Nationwide</option>
+                              <option  value="Greece - Local">Greece - Local</option>
+                              <option  value="Greece - WebTV">Greece - WebTV</option>
+                              <option  value="World - WebTV">World - WebTV</option>
+                              <option  value="Cyprus">Cyprus</option>
+                              <option  value="Cyprus - WebTV">Cyprus - WebTV</option>
+                            </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                      <label for="field-type-e" class="control-label">Type</label>
+                            <select id="field-type-e" class="form-control">
+                                <option  value="General">General</option>
+                                <option  value="News">News</option>
+                                <option  value="Music">Music</option>
+                            </select>
+                                    </div>
+                                </div>
+                            </div>
+
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="field-streamurl-e" class="control-label">Stream URL</label>
-                                        <input type="text" class="form-control" id="field-streamurl-e" placeholder="http://">
+                                        <label for="field-description-e" class="control-label">Description / Comment</label>
+                                        <input type="text" class="form-control" id="field-description-e">
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                      <label for="field-format-e" class="control-label">Format</label>
-                            <select id="field-format-e" class="form-control">
-                                <option value="hls">hls</option>
-                                <option value="youtube">youtube</option>
-                                <option value="mp4">mp4</option>
-                                <option value="podcast">podcast</option>
-                                <option value="ustream">ustream</option>
-                                <option value="other">other</option>
-                            </select>
+                                      <label for="field-sdimage-e" class="control-label">SD Image</label>
+                            <input type="text" class="form-control" id="field-sdimage-e">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                      <label for="field-active-e" class="control-label">Active</label>
-                            <select id="field-active-e" class="form-control">
-                                <option value="0">0</option>
-                                <option value="1" >1</option>
-                            </select>
+                                      <label for="field-hdimage-e" class="control-label">HD Image</label>
+                            <input type="text" class="form-control" id="field-hdimage-e">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                      <label for="field-hd-e" class="control-label">HD</label>
-                            <select id="field-hd-e" class="form-control">
-                                <option  value="false">false</option>
-                                <option  value="true">true</option>
+                                      <label for="field-order-e" class="control-label">Order</label>
+                            <select id="field-order-e" class="form-control">
+                              <?php
+                              for ($i = 1; $i <= 100; $i++) {echo '<option value="'.$i.'">'.$i.'</option>';}
+?>
                             </select>
                                     </div>
                                 </div>
@@ -165,8 +179,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
-                            <button type="submit" value="Submit"  class="btn btn-info waves-effect waves-light">Edit Stream</button>
-                            <input type="hidden" value="" id="field-streamid-e"/>
+                            <button type="submit" value="Submit"  class="btn btn-info waves-effect waves-light">Edit Channel</button>
                         </div>
                         </form>
                     </div>
