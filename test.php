25d2ceb8e2620da60ee7e324c28a1043992f1b9e
Ioannis Kokkinis <info@upg.gr>
2016-10-10 20:41:57 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,10 @@
 <link href="//vjs.zencdn.net/4.12/video-js.css" rel="stylesheet">
 <script src="//vjs.zencdn.net/4.12/video.js"></script>
 <script src="//cdn.viblast.com/vb/stable/viblast.js"></script>
-
 <video id="example_video_2" class="video-js vjs-default-skin"
   controls preload="auto" width="640" height="264"
   data-setup='{"techOrder": ["viblastflash", "html5"]}'
   data-viblast-key="N8FjNTQ3NDdhZqZhNGI5NWU5ZTI=">
- <source src="http://94.23.218.186:1935/cretatv/cretatv/playlist.m3u8" type='application/x-mpegURL' />
- <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+ <source src="http://stream.ssh101.com:1935/live/lampsitv/playlist.m3u8" type='application/x-mpegURL' />
+ <p class="vjs-no-js">greektv by upg.gr</p>
 </video>
