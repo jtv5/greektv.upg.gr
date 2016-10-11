16463361be3fda038f30f5dcb8fe166d4f195320
Ioannis Kokkinis <info@upg.gr>
2016-10-11 10:10:57 +0300

    Updated test.php.

--- test.php
+++ test.php
@@ -1,1 +1,1 @@
-<iframe src="http://greektv.upg.gr/upg_player.html?m3u8=http://antglantennatv-lh.akamaihd.net/i/live_1@421307/master.m3u8" width="800" height="600"></iframe>
+<iframe src="http://greektv.upg.gr/upg_player.html?m3u8=http://stream.ssh101.com:1935/live/greektv/playlist.m3u8" width="800" height="600"></iframe>
