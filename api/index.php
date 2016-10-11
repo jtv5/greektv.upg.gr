9508616b9dd08ecdda1b133fcb7e9537e444836a
Ioannis Kokkinis <info@upg.gr>
2016-10-11 10:05:23 +0300

    Updated index.php.

--- api/index.php
+++ api/index.php
@@ -3,7 +3,5 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'functions.php';
-
 startapi();
-// olympus addon source : https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/android.m3u
 ;
