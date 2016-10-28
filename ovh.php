78ab82b68c18a91d033c16cf65753321a44bdbe3
Ioannis Kokkinis <info@upg.gr>
2016-10-29 01:59:56 +0300

    Updated ovh.php.

---
+++ ovh.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+ ?>
+
+ delete window.document.referrer;
+ window.document.__defineGetter__('referrer', function () {
+     return "yoururl.com";
+ });
+
+ <iframe src="http://teste.mybeststream.xyz/pt/alphacyprus295/index.m3u8"?></iframe>
