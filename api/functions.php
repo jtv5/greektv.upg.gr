c6a3a93220993e666c6f54b791df4df4721cbe58
Ioannis Kokkinis <info@upg.gr>
2016-10-28 12:34:34 +0300

    f

--- api/functions.php
+++ api/functions.php
@@ -711,7 +711,7 @@ $chan = rtrim($chan);
 
 db_connect();
 $query ="select id, title from greekchannels WHERE title LIKE '$chan' LIMIT 1";
-//echo $query.'<br />';
+echo $query.'<br />';
 $result = db_query($query);
 if ($result === false) {
     return false;
