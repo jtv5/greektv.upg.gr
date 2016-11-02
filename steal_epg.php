52974c5ba0f0ba9a99e774f0864cca679e6b0dbe
Ioannis Kokkinis <info@upg.gr>
2016-11-02 15:57:04 +0200

    Updated steal_epg.php.

--- steal_epg.php
+++ steal_epg.php
@@ -1,4 +1,4 @@
-<form action="http://www.digea.gr/ajax_epg.php">
+<form action="http://www.digea.gr/ajax_epg.php" method-"POST">
   Tab:<br><input type="text" name="tab" value="Regional"><br>
 Last name:<br><input type="text" name="curdate1" value="2016-11-02 06:00:00"><br>
 First name:<br><input type="text" name="curdate2" value="2016-11-03 05:00:00"><br>
