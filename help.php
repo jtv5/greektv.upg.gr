4358655ec8af835bd682b61ec5ec7d4dd6df9b71
Ioannis Kokkinis <info@upg.gr>
2016-11-22 19:14:04 +0200

    Updated help.php.

--- help.php
+++ help.php
@@ -108,8 +108,12 @@ theval = theval.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 if (theval.substring(theval.length -4) == 'm3u8' && theval.substring(0,4) == 'http') {
   console.log('found m3u8 link');
 var a = document.createElement('a');
-a.href = theval;
-console.log(a.hostname);
+thehost = a.href
+theport = a.port
+theapplication = a.pathname
+console.log(thehost);
+console.log(theport);
+console.log(theapplication);
 }
 
 
