c92fae1676e7c5829c4c1fc6dcc8c4d06781ec64
Ioannis Kokkinis <info@upg.gr>
2016-11-22 20:31:50 +0200

    Updated help.php.

--- help.php
+++ help.php
@@ -118,11 +118,11 @@ if (therest.substring(therest.length -13) == 'playlist.m3u8'){
   let m;
   while ((m = regex.exec(therest)) !== null) {
     if (m.index === regex.lastIndex) {regex.lastIndex++;}
-    m.forEach((match) => {
-console.log(`Found match, ${match}`);
-      theapplication = m[0];
-      thestream = m[1];
- });
+
+
+      theapplication = m[3];
+      thestream = m[5];
+
   }
 }
 
