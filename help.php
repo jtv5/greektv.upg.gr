cb2fc69e25d31026705a90f9e1635b7ea62e08c6
Ioannis Kokkinis <info@upg.gr>
2016-11-22 19:11:51 +0200

    Updated help.php.

--- help.php
+++ help.php
@@ -100,8 +100,19 @@ $( document ).ready(function(){
 $('.entry').on('input', function() {
 //  console.log($('.entry').val());
 var theval = $('.entry').val();
+var thehost
+var theport
+var theapplication
+var thestream
 theval = theval.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-if (theval.substring(theval.length -4) == 'm3u8' && theval.substring(0,4) == 'http') {console.log('found m3u8 link');}
+if (theval.substring(theval.length -4) == 'm3u8' && theval.substring(0,4) == 'http') {
+  console.log('found m3u8 link');
+var a = document.createElement('a');
+a.href = theval;
+console.log(a.hostname);
+}
+
+
 })
 });
 </script>
