7b99693f14277ff4746e8d2ccf148fccc2fc457a
Ioannis Kokkinis <info@upg.gr>
2016-11-22 20:57:41 +0200

    Updated help.php.

--- help.php
+++ help.php
@@ -117,12 +117,8 @@ if (therest.substring(therest.length -13) == 'playlist.m3u8'){
   const regex = /([^/]*)\/[^/]*?/g;
   let m;
 m = therest.match(regex);
-//console.log(`Found match, group ${groupIndex}: ${match}`);
-console.log(m);
-theapplication = m[1];
-thestream = m[2];
-
-
+theapplication = m[1].substring(therest.length -1);
+thestream = m[2].substring(therest.length -1);
 }
 
 console.log(thehost);
