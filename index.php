a41a6354273106e59402ac059aae8d9c97f1908a
Ioannis Kokkinis <info@upg.gr>
2016-12-08 11:41:37 +0200

    Updated index.phpupg_player.html .

--- index.php
+++ index.php
@@ -170,15 +170,6 @@ $(document).ready(function(){
     		$(window).resize();
 });
 </script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-57648878-32', 'auto');
-  ga('send', 'pageview');
-
-</script>
+<script type='text/javascript' src='analytics.js'></script>
 </body>
 </html>
