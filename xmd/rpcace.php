a41ed233e50d02d301fafa7506195d55d40ba64f
Ioannis Kokkinis <info@upg.gr>
2017-02-12 01:51:23 +0200

    f

--- xmd/rpcace.php
+++ xmd/rpcace.php
@@ -4,7 +4,7 @@ require_once 'jsonRPCClient.php';
   $bitcoin = new jsonRPCClient('http://xmdcoin4.cloudapp.net:63667/json_rpc');
 
   echo "<pre>\n";
-  print_r($bitcoin->f_get_blockchain_settings()); echo "\n";
+  print_r($bitcoin->getblockhash()); echo "\n";
 //  echo "Received: ".$bitcoin->f_get_blockchain_settings()."\n";
   echo "</pre>";
 
