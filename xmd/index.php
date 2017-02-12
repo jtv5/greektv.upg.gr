ac93eddbd7fae45bcedd3756787ebf9614fe0ced
Ioannis Kokkinis <info@upg.gr>
2017-02-12 02:09:41 +0200

    f

--- xmd/index.php
+++ xmd/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include('JsonRpcClient.php');
+include('xmdrpc.php');
 
 $api = new JsonRpcClient('http://xmdcoin4.cloudapp.net:63667/json_rpc');
 print $api->account_balance('hash', '0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232');
