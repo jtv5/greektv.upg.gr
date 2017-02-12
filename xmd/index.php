f1500d7822ee9fb27b525c90bc198f464c2691c8
Ioannis Kokkinis <info@upg.gr>
2017-02-13 00:15:01 +0200

    d

--- xmd/index.php
+++ xmd/index.php
@@ -1,32 +1,23 @@
-<?php
-include('JsonRPC/Client.php');
-include('JsonRPC/HttpClient.php');
-include('JsonRPC/Request/RequestBuilder.php');
-include('JsonRPC/Request/RequestParser.php');
-include('JsonRPC/Response/ResponseParser.php');
-include('JsonRPC/Response/ResponseParser.php');
-use JsonRPC\Client;
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="rpc.js"></script>
+<script>
+$.jsonRPC.setup({
+  endPoint: 'http://xmdcoin4.cloudapp.net:63667/json_rpc',
+  namespace: 'xmd'
+});
 
-$client = new Client('http://xmdcoin4.cloudapp.net:63667/json_rpc');
-$client->getHttpClient()->withDebug();
-//$client->authentication('jsonrpc', '19ffd9709d03ce50675c3a43d1c49c1ac207f4bc45f06c5b2701fbdf8929');
-$result = $client->getblockheaderbyhash("hash", "0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232");
-print_r($result);
-
-
-//$api = new JsonRpcClient('http://xmdcoin4.cloudapp.net:63667/json_rpc');
-//echo "<pre>\n";
-//print_r($api->getblockheaderbyhash("hash", "0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232"));echo "\n";
-//echo "</pre>";
-
+var method = getblockheaderbyhash;
+var params =
+$.jsonRPC.request(method, {
+  params: ("hash", "0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232"),
+  success: function(result) {
+console.log(result);
+  },
+  error: function(result) {
+    console.log(result);
+  }
+});
 
 
 
-
-
-//  echo "<pre>\n";
-//  print_r($bitcoin->getblockheaderbyhash("hash","0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232")); echo "\n";
-//  echo "Received: ".$bitcoin->f_get_blockchain_settings()."\n";
-//  echo "</pre>";
-
-  ?>
+</script>
