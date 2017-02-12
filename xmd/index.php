aafca1bd860e9a2635e0ea1348a67cdbdba2498b
Ioannis Kokkinis <info@upg.gr>
2017-02-13 00:15:55 +0200

    d

--- xmd/index.php
+++ xmd/index.php
@@ -6,8 +6,7 @@ $.jsonRPC.setup({
   namespace: 'xmd'
 });
 
-var method = getblockheaderbyhash;
-var params =
+var method = 'getblockheaderbyhash';
 $.jsonRPC.request(method, {
   params: ("hash", "0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232"),
   success: function(result) {
