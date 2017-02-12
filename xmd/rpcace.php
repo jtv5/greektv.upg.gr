b3f2a0f408da0046adf45402dd2dac82557f9f8c
Ioannis Kokkinis <info@upg.gr>
2017-02-12 02:08:08 +0200

    d

--- xmd/rpcace.php
+++ xmd/rpcace.php
@@ -1,11 +1,19 @@
 <?php
-require_once 'jsonRPCClient.php';
 
-  $bitcoin = new jsonRPCClient('http://xmdcoin4.cloudapp.net:63667/json_rpc');
+include('JsonRpcClient.php');
 
-  echo "<pre>\n";
-  print_r($bitcoin->getblockheaderbyhash("hash","0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232")); echo "\n";
+$api = new JsonRpcClient('http://xmdcoin4.cloudapp.net:63667/json_rpc');
+print $api->account_balance('hash', '0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232');
+
+
+
+
+
+
+
+//  echo "<pre>\n";
+//  print_r($bitcoin->getblockheaderbyhash("hash","0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232")); echo "\n";
 //  echo "Received: ".$bitcoin->f_get_blockchain_settings()."\n";
-  echo "</pre>";
+//  echo "</pre>";
 
   ?>
