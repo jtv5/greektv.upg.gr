2942cbe83c9cb0663f73413d890e42085364c6e2
Ioannis Kokkinis <info@upg.gr>
2017-02-12 02:22:10 +0200

    k

--- xmd/index.php
+++ xmd/index.php
@@ -4,7 +4,7 @@ include('xmdrpc.php');
 
 $api = new JsonRpcClient('http://xmdcoin4.cloudapp.net:63667/json_rpc');
 echo "<pre>\n";
-print_r($api->getblockheaderbyhash('hash', '0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232'));echo "\n";
+print_r($api->getblockheaderbyhash("hash", "0f5cd7b89c8d25bab8a033e87e4e93225f8105de1572fe31f7f6ecb48363d232"));echo "\n";
 echo "</pre>";
 
 
