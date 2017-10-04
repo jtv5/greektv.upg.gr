12af7b6c8095877675ae48b4b5eb359a04cb0fe2
Ioannis Kokkinis <info@upg.gr>
2017-10-04 10:29:46 +0300

    Updated functions.php.

--- api/functions.php
+++ api/functions.php
@@ -459,6 +459,7 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $data = curl_exec($ch);
+echo $data;
 curl_close($ch);
 $re = "/<span itemprop=\"contentUrl\" href=\"(.*?)\">/";
 preg_match($re, $data, $matches);
