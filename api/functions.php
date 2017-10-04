41cf03620bd2fd10d0223341c7072ee004e9ed5e
Ioannis Kokkinis <info@upg.gr>
2017-10-04 10:35:04 +0300

    Updated functions.php.

--- api/functions.php
+++ api/functions.php
@@ -453,13 +453,17 @@ break;
 
 case 'skaiurl_redirected':
 $url = "www.skai.gr/player/tvlive/";
+$agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+
+
 $ch = curl_init();
-$timeout = 5;
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+curl_setopt($ch, CURLOPT_URL,$url);
 $data = curl_exec($ch);
-echo $data;
+//echo $data;
 curl_close($ch);
 $re = "/<span itemprop=\"contentUrl\" href=\"(.*?)\">/";
 preg_match($re, $data, $matches);
